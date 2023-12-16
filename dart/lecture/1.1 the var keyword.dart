e78a2806f05e53d66cf3bef52b8b5f9c9444daa1
S. Jung <sujin.jung.424@gmail.com>
2023-12-16 14:56:49 +0900

    var keyword

---
+++ dart/lecture/1.1 the var keyword.dart
@@ -0,0 +1,11 @@
+/* 1.1 The var Keyword 
+  - var : 함수, 메소드 내부에 지역 변수 선언할 때
+  - String과 같이 직접 변수타입 지정할 때 : class에서 변수, 프로퍼티 선언 시에
+*/
+
+void main() {
+  var name = '니꼬'; // var 키워드, 함수나 메소드 내부에 지역 변수 선언할 때
+  name = 'nico';
+  String name2 = 'nico2'; //class에서 변수나 property를 선언할 때 직접 변수타입 지정
+  print(name2);
+}
